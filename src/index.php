41004894ebd8bc7291f547a25c0dadef26d2f95c
Attalu <118429373+Attalu@users.noreply.github.com>
2023-04-11 09:33:29 +0000

    meme

--- src/index.php
+++ src/index.php
@@ -15,6 +15,31 @@
       <div class="form-signin">
         <div class="alert alert-success">You have been <strong>successfully logged in</strong>.</div>
         <a href="login/logout.php" class="btn btn-default btn-lg btn-block">Logout</a>
+        <h1>Miglior studente: Light Yagami</h1>
+        <p>BELLO
+INTELLIGENTE
+AFFASCINANTE
+SIMPATICO
+MODESTO
+FAMIGLIA PERFETTA
+FORTUNATO
+ENTUSIASTA
+AMOREVOLE
+TALENTUOSO
+EMPATICO
+SICURO DI SE
+ELEGANTE
+POLIEDRICO
+OBBIETTIVO
+ATLETICO
+COMPRENSIVO
+PERSUASIVO
+GENTILUOMO
+E AUTOCRITICOO</p>
+
+<iframe id="ytplayer" type="text/html" width="640" height="360"
+  src="https://www.youtube.com/embed/wOhiCFGOGBg"
+  frameborder="0"/>
       </div>
     </div> <!-- /container -->
   </body>
